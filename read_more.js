bdf4a5a2cf3f45ef4835e681b0b9bf238008ec38
Nico-MG <83792712+Nico-MG@users.noreply.github.com>
2022-11-06 15:17:32 -0300

    Delete read_more.js

--- read_more.js
+++
@@ -1,9 +0,0 @@
-let hideTextjs = document.getElementById('hideTextJs');
-let textHide = document.getElementById('hideText');
-
-hideTextjs.addEventListener('click',toggleText);
-function toggleText(){
-
-    textHide.classList.toggle('show');
-
-}

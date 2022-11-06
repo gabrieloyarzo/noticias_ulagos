ae9719baba0d070b19102f7e1fffc7d99487948c
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2022-11-06 15:56:55 -0300

    mostrar mas menos

--- src/ocultar.js
+++ src/ocultar.js
@@ -1,18 +1,22 @@
 limite = document.getElementsByClassName('noticia').length;
 
-for (i = 0; i <= limite-1; i++){
+for (i = 0; i <= limite - 1; i++) {
     text = i.toString()
-    boton = i+'button'
-    
-    
-    let hideText = document.getElementById(i);
-    let buttonHide = document.getElementById(boton);
-    buttonHide.addEventListener('click',toggleText)
-    
-    function toggleText(){
+    boton = i + 'button'
 
-    hideText.classList.toggle('show'); 
-    }
+
+    let hideText = document.getElementById(i)
+    let buttonHide = document.getElementById(boton)
+    
+    buttonHide.addEventListener('click', () => {
+        hideText.classList.toggle('show');
+        if (hideText.classList.contains('show')) {
+            buttonHide.innerHTML = 'Mostrar menos'
+        }
+        else {
+            buttonHide.innerHTML = 'Mostrar más'
+        }
+    })
 
 
 }

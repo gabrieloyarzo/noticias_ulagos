ed82ab0cea54b643516e07be96e10165cebf4322
F4B14NM4RT1N3Z <86687869+F4B14NM4RT1N3Z@users.noreply.github.com>
2022-11-06 01:45:28 -0300

    errores corregidos

--- src/conect_noticia.php
+++ src/conect_noticia.php
@@ -1,23 +1,19 @@
-
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script language= javascript type= text/javascript>
 function datosInvalidos(){
  swal({
-    title:'Credenciales incorrectas!',
+    title: 'Error',
+    text:'Credenciales incorrectas!',
     icon:'error'
 })
 }
-function bienvenido(){
+function noticiaPublicada(){
 swal({
-   title:'Bienvenido!',
+   title:'La noticia ha sido publicada!',
    icon:'success'
 })
 }
 </script> 
-
-
-
-
 
 <?php
 #Conexion a la base de datos
@@ -34,21 +30,23 @@ if (isset($_POST['send_noticia'])){
     $correo    = strval(trim($_POST['correo']));
     $contra    = strval(trim($_POST['contra']));
     
-    $datos = "SELECT * FROM usuario where correo = '$correo' and contrasena = '$contra'";
-    $resultado = mysqli_query($conexion,$datos);
-    $fila=mysqli_fetch_array($resultado);
-    echo $fila;
+    $datos = "SELECT * FROM usuario WHERE correo = '$correo' AND contrasena = '$contra'";
+    $resultado = mysqli_fetch_array(mysqli_query($conexion,$datos));
+    
 
     if (strval($titulo) >=1 && strval($contenido) >=1 && strval($categoria) >=1 && strval($fecha) >=1 && $resultado)
     {
-        #termporal para el correo
+       
 
         $dates = "INSERT INTO noticia (titulo,fecha,descripcion,correo,nombrec) values('$titulo','$fecha','$contenido','$correo','$categoria')";
         $result = mysqli_query($conexion,$dates);
+        echo '<script>';
+        echo 'noticiaPublicada()';
+        echo '</script>';
 
     } else {
         echo '<script>';
-        echo 'DatosInvalidos()';
+        echo 'datosInvalidos()';
         echo '</script>';
 
     }
